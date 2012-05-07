4d0c74a1ba71bac6108d2a94731be018218c8368
Andrew Sliwinski <thisandagain@gmail.com>
2012-05-06 18:50:31 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# Trebuchet
+#### Let's chuck some email!
+
+Trebuchet is a node.js module for super simple email transactions using the [Postmark API](http://postmarkapp.com) and [Mustache](http://mustache.github.com/) for templating. Trebuchet was designed with more complex email rendering, batching and API operations in mind... if your needs are different, there are lots of other [great](https://github.com/voodootikigod/postmark.js) [options](https://github.com/andris9/Nodemailer) in the NPM sea.
+
+## Installation
+    npm install trebuchet
+
+## Templating
+Trebuchet uses [Mustache](http://mustache.github.com/) templates to make sending dynamic HTML and plain-text emails super-duper simple. 
+
+An example template:
+```html
+<html>
+    <body>
+        <h1>{{greeting}}</h1>
+    </body>
+</html>
+````
+
+With example data:
+```javascript
+{
+    greeting: 'Hello World!'
+}
+````
+
+Result:
+```html
+<html>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
+````
+
+## Basic Use (Fling Pattern)
+```javascript
+var trebuchet = require('trebuchet')('yourapikey');
+
+trebuchet.fling({
+    params:     {
+        from:       'you@domain.com',
+        to:         'someone@domain.com',
+        subject:    'This is only a test of the fling pattern'
+    },
+    html:       'path/to/template/fling.html',
+    text:       'path/to/template/fling.txt',
+    data:       {
+        foo:        'Bar'
+    }
+}, function (err, response) {
+    // Win!
+});
+````
+
+## Batch Sender (Load --> Fire Pattern)
+The batch sender uses the [Postmark API's batch method](http://developer.postmarkapp.com/developer-build.html#batching-messages) to support sending groups of messages (up to 500 at a time).
+
+```javascript
+var trebuchet = require('trebuchet')('yourapikey');
+
+trebuchet.load({
+    params:     {
+        from:       'you@domain.com',
+        to:         'someone@domain.com',
+        subject:    'This is only a test of the load/fire pattern'
+    },
+    html:       'path/to/template/fire.html',
+    text:       'path/to/template/fire.txt',
+    data:       {
+        foo:        'Bar',
+        name:       'Bubba'
+    }
+}, function (err, response) {
+    // Loaded!
+});
+````
+
+```javascript
+trebuchet.fire(function (err, response) {
+    // Win! 
+});
+````
+
+## Testing
+    node test/* --from "you@domain.com" --to "someone@domain.com"
---
+++ test/index.js
@@ -0,0 +1,157 @@
+/**
+ * Unit test for load & fire methods.
+ *
+ * @package  trebuchet
+ * @author  Andrew Sliwinski <andrew@diy.org>
+ */
+var assert      = require('assert'),
+    vows        = require('vows'),
+    argv        = require('optimist')
+        .demand(['from','to'])
+        .default('key', 'POSTMARK_API_TEST')
+        .argv;
+
+/**
+ * Setup
+ */
+var suite       = vows.describe('Trebuchet');
+var trebuchet   = require('../lib/index.js')(argv.key);
+
+/**
+ * Suite
+ */
+suite
+.addBatch({
+    'Fling': {
+        
+        topic:  function() {
+            trebuchet.fling({
+                params:     {
+                    from:       argv.from,
+                    to:         argv.to,
+                    subject:    'This is only a test of the fling pattern'
+                },
+                html:       'test/template/fling.html',
+                text:       'test/template/fling.txt',
+                data:       {
+                    foo:        'Bar'
+                }
+            }, this.callback);
+        },
+
+        'does not return an error': function (err, obj) {
+            assert.isNull(err);
+        },
+
+        'returns a response': function (err, obj) {
+            assert.isNotNull(obj);  
+        },
+
+        'response is an object': function (err, obj) {
+            assert.isObject(obj);   
+        },
+
+        'response includes expected values from API': function (err, obj) {
+            assert.equal(obj.To, argv.to);
+            assert.equal(obj.ErrorCode, 0);
+        }
+
+    },
+})
+
+.addBatch({
+    'Load #1': {
+
+        topic:  function () {
+            trebuchet.load({
+                params:     {
+                    from:       argv.from,
+                    to:         argv.to,
+                    subject:    'This is only a test of the load/fire pattern #1'
+                },
+                html:       'test/template/fire.html',
+                text:       'test/template/fire.txt',
+                data:       {
+                    foo:        'Bar',
+                    name:       'Bubba'
+                }
+            }, this.callback);
+        },
+
+        'does not return an error': function (err, obj) {
+            assert.isNull(err);
+        },
+
+        'returns a response': function (err, obj) {
+            assert.isNotNull(obj);  
+        },
+
+        'response is a number': function (err, obj) {
+            assert.isNumber(obj);   
+        }
+
+    }
+})
+
+.addBatch({
+    'Load #2': {
+
+        topic:  function () {
+            trebuchet.load({
+                params:     {
+                    from:       argv.from,
+                    to:         argv.to,
+                    subject:    'This is only a test of the load/fire pattern #2'
+                },
+                html:       'test/template/fire.html',
+                text:       'test/template/fire.txt',
+                data:       {
+                    foo:        'Bar',
+                    name:       'Jane'
+                }
+            }, this.callback);
+        },
+
+        'does not return an error': function (err, obj) {
+            assert.isNull(err);
+        },
+
+        'returns a response': function (err, obj) {
+            assert.isNotNull(obj);  
+        },
+
+        'response is a number': function (err, obj) {
+            assert.isNumber(obj);   
+        }
+
+    },
+})
+
+.addBatch({    
+    'Fire': {
+        
+        topic:  function () {
+            trebuchet.fire(this.callback);
+        },
+
+        'does not return an error': function (err, obj) {
+            assert.isNull(err);
+        },
+
+        'returns a response': function (err, obj) {
+            assert.isNotNull(obj);  
+        },
+
+        'response is an array': function (err, obj) {
+            assert.isArray(obj);   
+        },
+
+        'response includes expected values from API': function (err, obj) {
+            assert.equal(obj[0].To, argv.to);
+            assert.equal(obj[0].ErrorCode, 0);
+        }
+
+    },
+})
+
+.export(module);
